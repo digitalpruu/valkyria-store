42b899459cf3ef5d8a8f071b740de09d49e66f23
kevinpicon01 <digitalpruu@gmail.com>
2024-09-27 02:24:56 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,10 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Valkyria Store";
+const email = ""; // No se proporcionó correo electrónico
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573208637250"; // Incluido el prefijo +57
+
 const textos = {
     index: {
         title: title,
@@ -12,25 +13,25 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En Valkyria Store, nos apasiona la moda y nos dedicamos a ofrecerte una selección exclusiva de ropa que destaca por su calidad y diseño. Cada prenda es cuidadosamente elegida para garantizar que encuentres la combinación perfecta entre estilo y comodidad.",
+        description2: "Nuestro compromiso es brindarte una experiencia de compra excepcional, donde cada cliente se sienta especial y valorado. Creemos que la moda es una forma de expresión, y en Valkyria Store, te ayudamos a expresar tu personalidad a través de nuestra colección única.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
         email: email
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Estilo y confort para todos.",
+            p2: ""
         },
-        address: "Dirección del emprendimiento",
+        address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/profile.php?id=100089100311273", // Enlace a Facebook proporcionado
+        instagram: "https://www.instagram.com/valkyriaxstore?igsh=MXBiam1sNWtramNnaA==", // Enlace a Instagram proporcionado
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
-
 };
+
 export default textos;
